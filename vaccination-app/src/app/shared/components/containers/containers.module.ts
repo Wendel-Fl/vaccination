74eedfcb21dfd37861be43aa7db35408d2e8527c
Robert-Martins <robertcantares@gmail.com>
2024-01-27 23:02:49 -0300

    feat/create-app-flex-container

--- vaccination-app/src/app/shared/components/containers/containers.module.ts
+++ vaccination-app/src/app/shared/components/containers/containers.module.ts
@@ -2,20 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VacGridComponent } from './vac-grid/vac-grid.component';
 import { VacPageComponent } from './vac-page/vac-page.component';
+import { VacFlexComponent } from './vac-flex/vac-flex.component';
 
 
 
 @NgModule({
   declarations: [
     VacGridComponent,
-    VacPageComponent
+    VacPageComponent,
+    VacFlexComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
     VacGridComponent,
-    VacPageComponent
+    VacPageComponent,
+    VacFlexComponent
   ]
 })
 export class ContainersModule { }
