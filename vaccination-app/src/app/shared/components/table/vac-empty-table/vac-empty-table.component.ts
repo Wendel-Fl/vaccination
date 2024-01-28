8847a96a8235be7ac6c8d9e9678a38a2d592235b
Robert-Martins <robertcantares@gmail.com>
2024-01-28 10:59:39 -0300

    feat/empty-table-compoponent

---
+++ vaccination-app/src/app/shared/components/table/vac-empty-table/vac-empty-table.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vac-empty-table',
+  template: `
+    <span>Não há registros desta entidade</span>
+  `,
+  styleUrl: './vac-empty-table.component.scss'
+})
+export class VacEmptyTableComponent {
+
+}
