6d96c2812b58206c60b9feea399c04ef9ed1f030
Robert-Martins <robertcantares@gmail.com>
2024-01-29 19:02:13 -0300

    feat/vaccine-crud-and-listing

--- vaccination-app/src/app/views/vaccine/vaccine-form/vaccine-form.component.ts
+++ vaccination-app/src/app/views/vaccine/vaccine-form/vaccine-form.component.ts
@@ -1,19 +1,160 @@
-import { Component, Injector } from '@angular/core';
+import { Component, Injector, OnDestroy, OnInit } from '@angular/core';
 import { UtilComponent } from '../../../core/utils/util.component';
+import { SharedModule } from '../../../shared/shared.module';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { VaccineService } from '../../../core/services/vaccine.service';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { ConfirmationDialogComponent } from '../../../shared/components/dialogs/confirmation-dialog/confirmation-dialog.component';
+import { Vaccine } from '../../../core/models/vaccine.model';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-vaccine-form',
   standalone: true,
-  imports: [],
+  imports: [SharedModule, FormsModule, ReactiveFormsModule],
   templateUrl: './vaccine-form.component.html',
   styleUrl: './vaccine-form.component.scss'
 })
-export class VaccineFormComponent extends UtilComponent {
+export class VaccineFormComponent extends UtilComponent implements OnInit, OnDestroy {
+
+  public vaccineForm$: BehaviorSubject<FormGroup> = new BehaviorSubject(null);
+
+  public newVaccine$: BehaviorSubject<boolean> = new BehaviorSubject(true);
 
   constructor(
+    private vaccineService: VaccineService,
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
     injector: Injector
   ) {
     super(injector);
+  }
+
+  public ngOnInit(): void {
+      this.getRouteData();
+  }
+
+  public ngOnDestroy(): void {
+    this.vaccineForm$.unsubscribe();
+    this.newVaccine$.unsubscribe();
+  }
+
+  public onSave(): void {
+    if(this.vaccineForm.valid) 
+      this.saveVaccine();
+    else
+      this.toastr.info("Formulário inválido!");
+  }
+
+  public onClickDelete(): void {
+    this.dialog.open(ConfirmationDialogComponent, {
+      inputs: {
+        text: 'Confirma deleção da Vacina?',
+      },
+      onClose: (bool: any) => this.handleDeletionConfirmation(bool),
+    });
+  }
+
+  private get vaccineForm(): FormGroup {
+    return this.vaccineForm$.value;
+  }
+
+  private handleDeletionConfirmation = (bool: any): void => {
+    if (bool) 
+      this.deleteVaccine();
+  };
+
+  private saveVaccine(): void {
+    this.loading.show();
+    const id: number = this.vaccineForm.controls['id'].value;
+    if(id)
+      this.updateVaccine();
+    else
+      this.createVaccine();
+  }
+
+  private createVaccine(): void {
+    this.vaccineService.create(this.vaccineForm.value)
+      .subscribe({
+        next: (vaccine: Vaccine) => {
+          this.toastr.success("Vacina cadastrada com sucesso!");
+          this.loading.hide();
+          this.router.navigate([`/vaccine/form/${vaccine?.id}`]);
+        },
+        error: this.handleError
+      })
+  }
+
+  private updateVaccine(): void {
+    this.vaccineService.update(this.vaccineForm.value)
+    .subscribe({
+      next: () => {
+        this.toastr.success("Vacina atualizada com sucesso!");
+        this.loading.hide();
+      },
+      error: this.handleError
+    })
+  }
+
+  private deleteVaccine(): void {
+    this.loading.show();
+    this.vaccineService.deleteById(this.vaccineForm?.controls['id']?.value)
+      .subscribe({
+        next: () => {
+          this.toastr.success("Vacina deletada com sucesso!");
+          this.router.navigate(['/vaccine']);
+        },
+        error: this.handleError
+      })
+  }
+
+  private handleRetrievedVaccineId(id: string): void {
+    if(Number.isNaN(id) || Number(id) === 0){
+      this.createVaccineForm();
+      this.loading.hide();
+    }
+    else {
+      this.newVaccine$.next(false);
+      this.findVaccineById(Number(id));
+    }
+  }
+
+  private findVaccineById(id: number): void {
+    this.loading.show();
+    this.vaccineService.findById(id)
+      .subscribe({
+        next: (vaccine: Vaccine) => {
+          this.createVaccineForm(vaccine);
+          this.loading.hide();
+        },
+        error: this.handleError
+      })
+  }
+
+  private createVaccineForm(vaccine: Vaccine = new Vaccine()): void {
+    this.vaccineForm$.next(
+      this.fb.group({
+        id: [vaccine?.id],
+        title: [vaccine?.title, [Validators.required, Validators.maxLength(60)]],
+        description: [vaccine?.description, [Validators.required, Validators.maxLength(200)]],
+        dosage: [vaccine?.dosage, [Validators.required]],
+        frequency: [vaccine?.frequency, [Validators.required]],
+        interval: [vaccine?.interval, [Validators.required]]
+      })
+    );
+  }
+
+  private getRouteData(): void {
+    this.loading.show();
+    this.route.paramMap.subscribe({
+      next: (map: ParamMap) => {
+        this.handleRetrievedVaccineId(map.get('id'));
+      },
+      error: () => {
+        this.toastr.error("Erro ao resgatar dados da rota");
+      }
+    });
   }
 
 }
--- vaccination-app/src/app/views/vaccine/vaccine-list/vaccine-list.component.ts
+++ vaccination-app/src/app/views/vaccine/vaccine-list/vaccine-list.component.ts
@@ -1,19 +1,80 @@
-import { Component, Injector } from '@angular/core';
+import { Component, Injector, OnDestroy, OnInit } from '@angular/core';
 import { UtilComponent } from '../../../core/utils/util.component';
+import { SharedModule } from '../../../shared/shared.module';
+import { Router, RouterModule } from '@angular/router';
+import { BehaviorSubject } from 'rxjs';
+import { Vaccine } from '../../../core/models/vaccine.model';
+import { VaccineService } from '../../../core/services/vaccine.service';
+import { ConfirmationDialogComponent } from '../../../shared/components/dialogs/confirmation-dialog/confirmation-dialog.component';
 
 @Component({
   selector: 'app-vaccine-list',
   standalone: true,
-  imports: [],
+  imports: [SharedModule, RouterModule],
   templateUrl: './vaccine-list.component.html',
   styleUrl: './vaccine-list.component.scss'
 })
-export class VaccineListComponent extends UtilComponent {
+export class VaccineListComponent extends UtilComponent implements OnInit, OnDestroy {
+
+  public vaccines$: BehaviorSubject<Vaccine[]> = new BehaviorSubject(null);
 
   constructor(
+    private vaccineService: VaccineService,
+    private router: Router,
     injector: Injector
   ) {
     super(injector);
+  }
+
+  public ngOnInit(): void {
+      this.loadVaccines();
+  }
+
+  public ngOnDestroy(): void {
+      this.vaccines$.unsubscribe();
+  }
+
+  public onClickEdit(id: number): void {
+    this.router.navigate([`/vaccine/form/${id}`]);
+  }
+
+  public onClickDelete(id: number): void {
+    this.dialog.open(ConfirmationDialogComponent, {
+      inputs: {
+        text: 'Confirma deleção da Vacina?',
+      },
+      onClose: (bool: any) => this.handleDeletionConfirmation(bool, id),
+    });
+  }
+
+  private handleDeletionConfirmation = (bool: any, id: number): void => {
+    if (bool) 
+      this.deleteVaccine(id);
+  };
+
+  private deleteVaccine(id: number): void {
+    this.loading.show();
+    this.vaccineService.deleteById(id)
+      .subscribe({
+        next: () => {
+          this.toastr.success("Vacina deletada com sucesso!");
+          this.loadVaccines();
+        },
+        error: this.handleError
+      })
+  }
+
+  private loadVaccines(): void {
+    this.loading.show();
+    this.vaccineService.findAll()
+      .subscribe({
+        next: (vaccines: Vaccine[]) => {
+          if(vaccines?.length > 0)
+            this.vaccines$.next(vaccines);
+          this.loading.hide();
+        },
+        error: this.handleError
+      })
   }
 
 }
