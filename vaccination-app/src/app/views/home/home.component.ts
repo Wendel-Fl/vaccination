bc58dc85b053f317e3d89c13bf300ffb3263cedd
Robert-Martins <robertcantares@gmail.com>
2024-01-31 19:17:13 -0300

    feat/vac-app-home

--- vaccination-app/src/app/views/home/home.component.ts
+++ vaccination-app/src/app/views/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ContainersModule } from '../../shared/components/containers/containers.module';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'vac-home',
   standalone: true,
-  imports: [ContainersModule],
+  imports: [ContainersModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
